af4ff2fea8ba8870c4809ad2de01c0527dedbf09
Hugo <hugochanteloup@icloud.com>
2024-11-22 10:07:35 +0100

    update

--- app.py
+++ app.py
@@ -1,23 +1,5 @@
 import streamlit as st
-import plotly.express as px
 
-# load data
-df = px.data.gapminder()
+st.button("click here")
 
-# Title
-st.title('Welcome on streamlit lecture')
-
-# show dataset sample
-st.write(df.sample(5))
-
-# create a slider 
-year = st.slider('Year', int(df.year.min()), int(df.year.max()), step=5)
-
-
-# display a chart 
-fig = px.scatter(df[df['year']==year], x="gdpPercap", y="lifeExp",
-	         size="pop", color="continent",
-                 hover_name="country", log_x=True, size_max=60)
-
-
-st.plotly_chart(fig)        
+st.balloons('works')
